15812b03c355a7f86ee67272aeb076c2ac5c4dc1
dev <costaluciane087@gmail.com>
2023-09-19 16:41:13 -0300

    Desafio Concluido

--- src/SomaPimentoes.java
+++ src/SomaPimentoes.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SomaPimentoes {
+import java.util.Scanner;
+
+public class SomaPimentoes {
+    public static void main(String[] args) {
+        Scanner valorPimentoesA = new Scanner(System.in);
+        System.out.println("Valor de Pimentões Amarelo:");
+        int A = valorPimentoesA.nextInt();
+        System.out.println("Valor de Pimentões Vermelho: ");
+        Scanner valorPimentoesB = new Scanner(System.in);
+        int B = valorPimentoesB.nextInt();
+
+        int X = A + B;
+        System.out.println("X =" + X);
+    }
 }
